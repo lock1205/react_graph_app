0b3f88f52cd915c1e77c8a9e7f21e38e0d47b11a
lock1205 <mindheal205@gmail.com>
2024-08-07 19:07:13 +0900

    second commit

---
+++ back/index.js
@@ -0,0 +1,35 @@
+const PORT = 8081;
+const express = require('express'); //api 설정
+const cors = require('cors'); //http 통신을 위한 cors 설정
+
+const database = require('./services/database'); //데이터베이스 설정
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  //request를 보내면 response를 받아서 실행
+  res.send('Helo world'); //req를 받아오는건 없고 res 로 받은걸 화면에 보여줌
+});
+
+app.get('/visitors', async (req, res) => {
+  try {
+    const result = await database.pool.query('SELECT * FROM visitors');
+    return res.status(200).json(result.rows);
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
+  }
+});
+
+app.get('/customers', async (req, res) => {
+  try {
+    const result = await database.pool.query('SELECT * FROM customers');
+    return res.status(200).json(result.rows);
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
+  }
+});
+
+app.listen(PORT, () => console.log(`server running on port ${PORT}`)); //서버실행
---
+++ front/src/App.js
@@ -0,0 +1,53 @@
+// import { useEffect, useState } from 'react';
+
+import Appbar from './components/baselayout/Appbar';
+import Sidebar from './components/baselayout/Sidebar';
+import DashboardScreen from './components/dashboard/DashboardScreen';
+
+function App() {
+  // const [visitors, setVisitors] = useState([]);
+  // const [customers, setCustomers] = useState([]);
+
+  // useEffect(() => {
+  //   const fetchVisitors = async () => {
+  //     try {
+  //       const response = await fetch('http://3.27.35.124/visitors');
+  //       //console.log(response);
+  //       const visitors = await response.json();
+  //       setVisitors(visitors);
+  //     } catch (error) {
+  //       console.log(error);
+  //     }
+  //   };
+  //   const fetchCustomers = async () => {
+  //     try {
+  //       const response = await fetch('http://localhost:8081/customers');
+  //       //console.log(response);
+  //       const customers = await response.json();
+  //       setCustomers(customers);
+  //     } catch (error) {
+  //       console.log(error);
+  //     }
+  //   };
+
+  //   fetchCustomers();
+  //   fetchVisitors();
+  // }, []);
+
+  // console.log(visitors);
+  // console.log(customers);
+
+  return (
+    <div className="App w-full bg-[#212121] text-white">
+      <div className="page_wrapper h-screen flex">
+        <Sidebar />
+        <div className="content-wrapper ">
+          <Appbar />
+          <DashboardScreen />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
